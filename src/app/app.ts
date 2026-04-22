5ed96efd713162df19c456082d4b663b7fcbcb07
Davide Mininni <davide.mininni@finconsgroup.com>
2026-04-22 11:48:37 +0200

    feat: timetable swap button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {SbbSignet} from '@sbb-esta/lyne-angular/signet';
+import {
+  SbbTimetableForm,
+  SbbTimetableFormDetails,
+  SbbTimetableFormField,
+  SbbTimetableFormSwapButton
+} from '@sbb-esta/lyne-angular/timetable-form';
+import {SbbAutocomplete} from '@sbb-esta/lyne-angular/autocomplete';
+import {SbbFormField} from '@sbb-esta/lyne-angular/form-field';
+import {SbbDateInput} from '@sbb-esta/lyne-angular/date-input';
+import {
+  SbbDatepicker,
+  SbbDatepickerNextDay,
+  SbbDatepickerPreviousDay,
+  SbbDatepickerToggle
+} from '@sbb-esta/lyne-angular/datepicker';
+import {SbbDivider} from '@sbb-esta/lyne-angular/divider';
+import {SbbTimeInput} from '@sbb-esta/lyne-angular/time-input';
+import {SbbToggle, SbbToggleOption} from '@sbb-esta/lyne-angular/toggle';
+import {SbbButton} from '@sbb-esta/lyne-angular/button';
+import {SbbTeaser} from '@sbb-esta/lyne-angular/teaser';
+import {SbbTitle} from '@sbb-esta/lyne-angular/title';
+import {SbbOption} from '@sbb-esta/lyne-angular/option';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SbbSignet, SbbTimetableForm, SbbTimetableFormField, SbbAutocomplete, SbbTimetableFormSwapButton, SbbTimetableFormDetails, SbbFormField, SbbDateInput, SbbDatepickerPreviousDay, SbbDatepickerNextDay, SbbDatepickerToggle, SbbDatepicker, SbbDivider, SbbTimeInput, SbbToggle, SbbToggleOption, SbbButton, SbbTeaser, SbbTitle, SbbOption],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
